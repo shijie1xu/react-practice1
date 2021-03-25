b3c710eca2b2f8896615aab8d5068d8b15d444dc
shijie1xu <43320663+shijie1xu@users.noreply.github.com>
2021-03-25 13:30:35 -0400

    cart done

--- src/Cart.js
+++ src/Cart.js
@@ -1,6 +1,12 @@
 import React,{Component} from "react";
 
 class Cart extends Component{
+    constructor(props) {
+        super(props);
+        this.state={
+            showCart: this.props.showCart
+        }
+    }
     render(){
         return(
             <div>
@@ -9,7 +15,7 @@ class Cart extends Component{
                         <th>Item</th>
                         <th>Quantity</th>
                         <th>Price</th>
-                        <th><a href="">Back to Store</a></th>
+                        <th><a href="#0" onClick={() => this.props.parentCallback(!this.state.showCart)}>Back to Store</a></th>
                     </tr>
                 </table>
             </div>
--- src/Items.js
+++ src/Items.js
@@ -1,50 +1,60 @@
 import React, {Component} from 'react';
-import { BrowserRouter as Router,Route, Switch} from 'react-router-dom';
+
 import Cart from "./Cart";
-import {Link} from "react-router-dom";
+
 
 
 class Items extends Component{
+
+    constructor(props) {
+        super(props);
+        this.state ={
+            showCart:false
+        }
+    }
+    callbackFunction = (childData) =>{
+        this.setState({showCart: childData})
+    }
     render() {
+
         return (
             <div>
-                <Router>
-                    <Switch>
-                        <Route path="/Cart" component={Cart}/>
-                    </Switch>
+                {!this.state.showCart && <div className="item">
+                    <input type="text" placeholder="Search"/>
+                    <br/>
+                    <span>Cart:0 Total Price:$0</span>
+                    <table>
+                        <tr>
+                            <th>Name</th>
+                            <th>Description</th>
+                            <th>Price</th>
+                            <th><a href="#0" onClick={() => this.setState({showCart: !this.state.showCart})}>My Cart</a></th>
+                        </tr>
+                        <tr>
+                            <td><a href="">Apple</a></td>
+                            <td>Eat one everyday, may keep the doctor away</td>
+                            <td>$12</td>
+                            <td><button>Add to cart</button></td>
+                        </tr>
+                        <tr>
+                            <td><a href="">Grape</a></td>
+                            <td>Wine is great, but grape is even better</td>
+                            <td>$11</td>
+                            <td><button>Add to cart</button></td>
+                        </tr>
+                        <tr>
+                            <td><a href="">Pineapple</a></td>
+                            <td>Enjoy but don't forget to peer first</td>
+                            <td>$8</td>
+                            <td><button>Add to cart</button></td>
+                        </tr>
+                    </table>
+                </div>}
 
+                <div className="cart">
+                    {this.state.showCart && <Cart showCart = {this.state.showCart} parentCallback = {this.callbackFunction}/>}
+                </div>
 
-                </Router>
-
-                <input type="text" placeholder="Search"/>
-                <br/>
-                <span>Cart:0 Total Price:$0</span>
-                <table>
-                    <tr>
-                        <th>Name</th>
-                        <th>Description</th>
-                        <th>Price</th>
-                        <th><a href="/Cart">My Cart</a></th>
-                    </tr>
-                    <tr>
-                        <td><a href="">Apple</a></td>
-                        <td>Eat one everyday, may keep the doctor away</td>
-                        <td>$12</td>
-                        <td><button>Add to cart</button></td>
-                    </tr>
-                    <tr>
-                        <td><a href="">Grape</a></td>
-                        <td>Wine is great, but grape is even better</td>
-                        <td>$11</td>
-                        <td><button>Add to cart</button></td>
-                    </tr>
-                    <tr>
-                        <td><a href="">Pineapple</a></td>
-                        <td>Enjoy but don't forget to peer first</td>
-                        <td>$8</td>
-                        <td><button>Add to cart</button></td>
-                    </tr>
-                </table>
             </div>
         );
     }
