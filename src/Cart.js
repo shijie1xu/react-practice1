5fcc67c3aceb58fa11ab1cb14fc4465a43437c9c
shijie1xu <43320663+shijie1xu@users.noreply.github.com>
2021-03-28 12:15:36 -0400

    fix bugs

--- src/Cart.js
+++ src/Cart.js
@@ -4,9 +4,118 @@ class Cart extends Component{
     constructor(props) {
         super(props);
         this.state={
-            showCart: this.props.showCart
+            showCart: this.props.showCart,
+            cartNumber: this.props.cartNumber,
+            appleNum: this.props.appleNum,
+            grapeNum: this.props.grapeNum,
+            pineappleNum: this.props.pineappleNum,
+            totalPrice: this.props.totalPrice
         }
+        this.applePlusOne = this.applePlusOne.bind(this)
+        this.appleMinusOne = this.appleMinusOne.bind(this)
+        this.appleDelete = this.appleDelete.bind(this)
+        this.grapePlusOne = this.grapePlusOne.bind(this)
+        this.grapeMinusOne = this.grapeMinusOne.bind(this)
+        this.grapeDelete = this.grapeDelete.bind(this)
+        this.pineapplePlusOne = this.pineapplePlusOne.bind(this)
+        this.pineappleMinusOne = this.pineappleMinusOne.bind(this)
+        this.pineappleDelete = this.pineappleDelete.bind(this)
+
     }
+
+    applePlusOne(){
+
+        this.setState({
+            appleNum: this.state.appleNum+1,
+            cartNumber: this.state.cartNumber+1,
+            totalPrice: this.state.totalPrice+12
+        });
+        this.props.setCart(this.state.cartNumber+1);
+        this.props.setApple(this.state.appleNum+1);
+        this.props.setPrice(this.state.totalPrice+12);
+
+    }
+    appleMinusOne(){
+        this.setState({
+            appleNum: this.state.appleNum-1,
+            cartNumber: this.state.cartNumber-1,
+            totalPrice: this.state.totalPrice-12
+        });
+        this.props.setCart(this.state.cartNumber-1);
+        this.props.setApple(this.state.appleNum-1);
+        this.props.setPrice(this.state.totalPrice-12);
+    }
+    appleDelete(){
+        this.setState({
+            cartNumber: this.state.cartNumber - this.state.appleNum,
+            appleNum: 0,
+            totalPrice: this.state.totalPrice - this.state.appleNum*12
+        });
+        this.props.setCart(this.state.cartNumber - this.state.appleNum);
+        this.props.setApple(0);
+        this.props.setPrice(this.state.totalPrice - this.state.appleNum*12);
+    }
+    grapePlusOne(){
+        this.setState({
+            grapeNum: this.state.grapeNum+1,
+            cartNumber: this.state.cartNumber+1,
+            totalPrice: this.state.totalPrice+11
+        });
+        this.props.setCart(this.state.cartNumber+1);
+        this.props.setGrape(this.state.grapeNum+1);
+        this.props.setPrice(this.state.totalPrice+11);
+    }
+    grapeMinusOne(){
+        this.setState({
+            grapeNum: this.state.grapeNum-1,
+            cartNumber: this.state.cartNumber-1,
+            totalPrice: this.state.totalPrice-11
+        });
+        this.props.setCart(this.state.cartNumber-1);
+        this.props.setGrape(this.state.grapeNum-1);
+        this.props.setPrice(this.state.totalPrice -11);
+    }
+    grapeDelete(){
+        this.setState({
+            cartNumber: this.state.cartNumber - this.state.grapeNum,
+            grapeNum: 0,
+            totalPrice: this.state.totalPrice - this.state.grapeNum*11
+        });
+        this.props.setCart(this.state.cartNumber - this.state.grapeNum);
+        this.props.setGrape(0);
+        this.props.setPrice(this.state.totalPrice - this.state.grapeNum*11);
+    }
+    pineapplePlusOne(){
+        this.setState({
+            pineappleNum: this.state.pineappleNum+1,
+            cartNumber: this.state.cartNumber+1,
+            totalPrice: this.state.totalPrice+8
+        });
+        this.props.setCart(this.state.cartNumber+1);
+        this.props.setPineapple(this.state.pineappleNum+1);
+        this.props.setPrice(this.state.totalPrice+8);
+    }
+    pineappleMinusOne(){
+        this.setState({
+            pineappleNum: this.state.pineappleNum-1,
+            cartNumber: this.state.cartNumber-1,
+            totalPrice: this.state.totalPrice-8
+        });
+        this.props.setCart(this.state.cartNumber-1);
+        this.props.setPineapple(this.state.pineappleNum-1);
+        this.props.setPrice(this.state.totalPrice-8);
+    }
+    pineappleDelete(){
+        this.setState({
+            cartNumber: this.state.cartNumber - this.state.pineappleNum,
+            pineappleNum: 0,
+            totalPrice: this.state.totalPrice - this.state.pineappleNum*8
+        });
+        this.props.setCart(this.state.cartNumber - this.state.pineappleNum);
+        this.props.setPineapple(0);
+        this.props.setPrice(this.state.totalPrice - this.state.pineappleNum*8);
+    }
+
     render(){
         return(
             <div>
@@ -16,6 +125,32 @@ class Cart extends Component{
                         <th>Quantity</th>
                         <th>Price</th>
                         <th><a href="#0" onClick={() => this.props.parentCallback(!this.state.showCart)}>Back to Store</a></th>
+                    </tr>
+                    <tr id="appleRow">
+                        <td>Apple</td>
+                        <td><button onClick={this.applePlusOne}>+</button>{this.state.appleNum}
+                            {this.state.appleNum!=0 && <button onClick={this.appleMinusOne}>-</button>}</td>
+                        <td>${this.state.appleNum*12}</td>
+                        <td><button onClick={this.appleDelete}>x</button></td>
+                    </tr>
+                    <tr id="grapeRow">
+                        <td>Grape</td>
+                        <td><button onClick={this.grapePlusOne}>+</button>{this.state.grapeNum}
+                            {this.state.grapeNum!=0 && <button onClick={this.grapeMinusOne}>-</button>}</td>
+                        <td>${this.state.grapeNum*11}</td>
+                        <td><button onClick={this.grapeDelete}>x</button></td>
+                    </tr>
+                    <tr id="pineappleRow">
+                        <td>Pineapple</td>
+                        <td><button onClick={this.pineapplePlusOne}>+</button>{this.state.pineappleNum}
+                            {this.state.pineappleNum!=0 && <button onClick={this.pineappleMinusOne}>-</button>}</td>
+                        <td>${this.state.pineappleNum*8}</td>
+                        <td><button onClick={this.pineappleDelete}>x</button></td>
+                    </tr>
+                    <tr>
+                        <td>total</td>
+                        <td>{this.state.cartNumber}</td>
+                        <td>${this.state.totalPrice}</td>
                     </tr>
                 </table>
             </div>
--- src/Items.js
+++ src/Items.js
@@ -4,17 +4,74 @@ import Cart from "./Cart";
 
 
 
+
 class Items extends Component{
+
 
     constructor(props) {
         super(props);
         this.state ={
-            showCart:false
+            showCart:false,
+            cartNumber:0,
+            appleNum: 0,
+            grapeNum: 0,
+            pineappleNum: 0,
+            totalPrice:0
         }
+
     }
+
+
+
+    addOneCart(){
+        this.setState({cartNumber: this.state.cartNumber +1})
+    }
+
+    addOneApple(){
+        this.setState(
+            {
+                appleNum: this.state.appleNum+1,
+                totalPrice: this.state.totalPrice+12
+        })
+    }
+    addOneGrape(){
+        this.setState({
+            grapeNum: this.state.grapeNum+1,
+            totalPrice: this.state.totalPrice+11
+        })
+    }
+
+    addOnePineapple(){
+        this.setState({
+            pineappleNum: this.state.pineappleNum+1,
+            totalPrice: this.state.totalPrice+8
+        })
+    }
+
     callbackFunction = (childData) =>{
         this.setState({showCart: childData})
     }
+
+    setCart = (cartNumber) =>{
+        this.setState({cartNumber: cartNumber})
+    }
+
+    setApple = (appleNumber) =>{
+        this.setState({appleNum: appleNumber})
+    }
+
+    setGrape = (grapeNumber) =>{
+        this.setState({grapeNum: grapeNumber})
+    }
+
+    setPineapple = (pineappleNumber) =>{
+        this.setState({pineappleNum: pineappleNumber})
+    }
+
+    setPrice = (price) =>{
+        this.setState({totalPrice: price})
+    }
+
     render() {
 
         return (
@@ -22,7 +79,7 @@ class Items extends Component{
                 {!this.state.showCart && <div className="item">
                     <input type="text" placeholder="Search"/>
                     <br/>
-                    <span>Cart:0 Total Price:$0</span>
+                    <span>Cart:{this.state.cartNumber} Total Price:${this.state.totalPrice}</span>
                     <table>
                         <tr>
                             <th>Name</th>
@@ -31,28 +88,42 @@ class Items extends Component{
                             <th><a href="#0" onClick={() => this.setState({showCart: !this.state.showCart})}>My Cart</a></th>
                         </tr>
                         <tr>
-                            <td><a href="">Apple</a></td>
+                            <td><a href="#0">Apple</a></td>
                             <td>Eat one everyday, may keep the doctor away</td>
                             <td>$12</td>
-                            <td><button>Add to cart</button></td>
+                            <td><button onClick={()=>{this.addOneCart(); this.addOneApple()}}>Add to cart</button></td>
                         </tr>
                         <tr>
-                            <td><a href="">Grape</a></td>
+                            <td><a href="#0">Grape</a></td>
                             <td>Wine is great, but grape is even better</td>
                             <td>$11</td>
-                            <td><button>Add to cart</button></td>
+                            <td><button onClick={()=>{this.addOneCart();this.addOneGrape()}}>Add to cart</button></td>
                         </tr>
                         <tr>
-                            <td><a href="">Pineapple</a></td>
+                            <td><a href="#0">Pineapple</a></td>
                             <td>Enjoy but don't forget to peer first</td>
                             <td>$8</td>
-                            <td><button>Add to cart</button></td>
+                            <td><button onClick={()=>{this.addOneCart();this.addOnePineapple()}}>Add to cart</button></td>
                         </tr>
                     </table>
                 </div>}
 
                 <div className="cart">
-                    {this.state.showCart && <Cart showCart = {this.state.showCart} parentCallback = {this.callbackFunction}/>}
+                    {this.state.showCart &&
+                    <Cart showCart = {this.state.showCart}
+                          cartNumber = {this.state.cartNumber}
+                          appleNum = {this.state.appleNum}
+                          grapeNum = {this.state.grapeNum}
+                          pineappleNum = {this.state.pineappleNum}
+                          totalPrice = {this.state.totalPrice}
+                          parentCallback = {this.callbackFunction}
+                          setCart = {this.setCart}
+                          setApple = {this.setApple}
+                          setGrape = {this.setGrape}
+                          setPineapple = {this.setPineapple}
+                          setPrice = {this.setPrice}
+                    />
+                    }
                 </div>
 
             </div>
